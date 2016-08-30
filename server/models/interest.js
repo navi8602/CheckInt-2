c211b1db7effeb011a9c1bd8ce030c709cc1a8c8
gr.tsvetkov <gr.tsvetkov@gmail.com>
2016-08-31 00:37:58 +0300

    for push

--- server/models/interest.js
+++ server/models/interest.js
@@ -1,3 +1,46 @@
+var pushSend = function(user_id, title, text) {
+
+    Push.debug = true;
+
+    let token = Meteor.users.findOne({_id: user_id});
+
+    if(token && token.profile && token.profile.token) {
+        token = token.profile.token;
+    } else {
+        return false;
+    }
+
+    let doc = Push.appCollection.findOne({
+        $and: [
+            { token: token },     // Match token
+            { appName: 'checkInt' }, // Match appName
+            { token: { $exists: true } }  // Make sure token exists
+        ]
+    });
+
+    if (!doc) {
+        Push.appCollection.insert({
+            token: token,
+            appName: 'checkInt',
+            userId: Meteor.userId(),
+            enabled: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+    }
+
+    Push.send({
+        from: 'push',
+        title: title,
+        text: text,
+        badge: 1, //optional, use it to set badge count of the receiver when the app is in background.
+        token: token
+        // tokens: array of appId's or tokens
+        // payload: user data
+        // delayUntil: Date
+    });
+};
+
 
 Meteor.methods({
 
