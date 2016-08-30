b56de649898959b0e562d73f228b513a1f3c990b
gr.tsvetkov <gr.tsvetkov@gmail.com>
2016-08-30 17:59:41 +0300

    for push

--- server/models/interest.js
+++ server/models/interest.js
@@ -8,14 +8,21 @@ Meteor.methods({
             phone = '+7'+phone.substring(1);
         }
 
-
         Interest.upsert({user_id: Meteor.userId(), to_phone: phone}, { $set: {
             name: name,
             to_name: to_name,
             to_id: to_id,
             from_phone: Meteor.user().phone.number,
             status:'Запрос отправлен'}
-        } )
+        });
+
+        let dataForPush = Meteor.users.findOne({'phone.number': phone});
+
+        if(dataForPush) {
+            if(dataForPush.profile && dataForPush.profile.token) {
+                pushSend(dataForPush._id, 'К Вам появили интерес!', 'Один из пользователей проявил к Вам интерес!');
+            }
+        }
     },
 
     'interest.remove': function(_id) {
--- server/models/push.js
+++ server/models/push.js
@@ -1,48 +1,47 @@
-Meteor.methods({
+var pushSend = function(user_id, title, text) {
 
-    'push.send': function(user_id, title, text) {
+    Push.debug = true;
 
-        Push.debug = true;
+    let token = Meteor.users.findOne({_id: user_id});
 
-        let token = Meteor.users.findOne({_id: user_id});
+    if(token && token.profile && token.profile.token) {
+        token = token.profile.token;
+    } else {
+        return false;
+    }
 
-        if(token && token.profile && token.profile.token) {
-            token = token.profile.token;
-        } else {
-            return false;
-        }
+    let doc = Push.appCollection.findOne({
+        $and: [
+            { token: token },     // Match token
+            { appName: 'checkInt' }, // Match appName
+            { token: { $exists: true } }  // Make sure token exists
+        ]
+    });
 
-        //let token = {apn: "a0d909333468c9afe88910031e72158aea6a8caff08143b349507f7252b0ef3c"};
-
-        let doc = Push.appCollection.findOne({
-            $and: [
-                { token: token },     // Match token
-                { appName: 'checkInt' }, // Match appName
-                { token: { $exists: true } }  // Make sure token exists
-            ]
-        });
-
-        if (!doc) {
-            Push.appCollection.insert({
-                token: token,
-                appName: 'checkInt',
-                userId: Meteor.userId(),
-                enabled: true,
-                createdAt: new Date(),
-                updatedAt: new Date()
-            });
-        }
-
-        Push.send({
-            from: 'push',
-            title: title,
-            text: text,
-            badge: 1, //optional, use it to set badge count of the receiver when the app is in background.
-            token: token
-            // tokens: array of appId's or tokens
-            // payload: user data
-            // delayUntil: Date
+    if (!doc) {
+        Push.appCollection.insert({
+            token: token,
+            appName: 'checkInt',
+            userId: Meteor.userId(),
+            enabled: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
         });
     }
 
+    Push.send({
+        from: 'push',
+        title: title,
+        text: text,
+        badge: 1, //optional, use it to set badge count of the receiver when the app is in background.
+        token: token
+        // tokens: array of appId's or tokens
+        // payload: user data
+        // delayUntil: Date
+    });
+};
+
+
+Meteor.methods({
+    'push.send': pushSend
 });
